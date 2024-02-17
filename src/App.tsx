c0d797d06c7436ababb264aec0c555a8f79a848c
athreya-harshith <athreya1207@gmail.com>
2024-02-17 14:36:23 +0530

    Updating the state of nested-objects

--- src/App.tsx
+++ src/App.tsx
@@ -6,17 +6,32 @@ function App() {
     price: 5,
   });
 
+  const [customer, setCustomer] = useState({
+    name: "athreya",
+    address: {
+      city: "Bengaluru",
+      zipCode: 560098,
+    },
+  });
   const handleClick = () => {
-    const newDrink = {
-      ...drink, // incase if the object has many properties then we can do like this
-      price: 10,
-    };
-    setDrink(newDrink);
+    // const newDrink = {
+    //   ...drink, // incase if the object has many properties then we can do like this
+    //   price: 10,
+    // };
+    // setDrink(newDrink);
+    setCustomer({
+      ...customer,
+      address: {
+        ...customer.address,
+        zipCode: 571401,
+      },
+    });
   };
   return (
     <div>
       <button onClick={handleClick}>Click</button>
-      <p>The Price of drink is {drink.price}</p>
+      {/* <p>The Price of drink is {drink.price}</p> */}
+      <p>The Zip code updated is {customer.address.zipCode}</p>
     </div>
   );
 }
