11df11d4f710bf8b3c57b431113bf4c8c8255947
athreya-harshith <athreya1207@gmail.com>
2024-02-17 21:08:52 +0530

    Updating the state of array of objects

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,10 @@ function App() {
     },
   });
   const [tags, setTags] = useState(["happy ", "cheerful"]);
-
+  const [bugs, setBug] = useState([
+    { id: 1, title: "Bug 1", fixed: false },
+    { id: 2, title: "Bug 2", fixed: false },
+  ]);
   const handleClick = () => {
     // const newDrink = {
     //   ...drink, // incase if the object has many properties then we can do like this
@@ -28,24 +31,21 @@ function App() {
     //     zipCode: 571401,
     //   },
     // });
-
     // Managing the State of an Array
     // Adding the elements
     // setTags([...tags, "excited"]);
     //removing the elements
-
     // setTags(tags.filter((tag) => tag !== "happy")); // returns true for all except the happy
     //updating
-
-    setTags(tags.map((tag) => (tag === "happy" ? "happyness" : tag)));
+    // setTags(tags.map((tag) => (tag === "happy" ? "happyness" : tag)));
+    setBug(bugs.map((bug) => (bug.id === 1 ? { ...bug, fixed: true } : bug)));
   };
+
+  let bugTitle = bugs[0].fixed ? "Fixed Bug " : "Not fixed";
   return (
     <div>
       <button onClick={handleClick}>Click</button>
-      {tags.length === 0 && <p>No items</p>}
-      {tags.map((tag) => (
-        <p>{tag}</p>
-      ))}
+      <p>Bug 1 Status : {bugTitle}</p>
     </div>
   );
 }
